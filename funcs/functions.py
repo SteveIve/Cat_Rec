0b846fcb8dbf6b4e1ba3a1cdb24469b274549c6f
Steve <steveive36@gmail.com>
2021-09-17 19:58:54 +0800

    add Cat_Rec.py add func predict, model

--- funcs/functions.py
+++ funcs/functions.py
@@ -121,3 +121,48 @@ def optimize(w, b, X, Y, num_iteration, learning_rate, print_cost=False):
              "db": db}
 
     return params, grads, costs
+
+
+def predict(w, b, X):
+    m = X.shape[1]
+    Y_prediction = np.zeros((1, m))
+    w = w.reshape(X.shape[0], 1)
+    A = sigmoid(np.dot(w.T, X) + b)
+
+    for i in range(A.shape[1]):
+        if (A[0, i] <= 0.5):
+            Y_prediction[0, i] = 0
+        else:
+            Y_prediction[0, i] = 1
+
+    assert (Y_prediction.shape == (1, m))
+
+    return Y_prediction
+
+
+def model(X_train, Y_train, X_test, Y_test,
+          num_iterations=2000, learning_rate=0.5,
+          print_cost=False):
+    w, b = initialize_with_zeros(X_train.shape[0])
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations,
+                                        learning_rate, print_cost=False)
+    w = parameters['w']
+    b = parameters['b']
+
+    Y_prediction_train = predict(w, b, X_train)
+    Y_prediction_test = predict(w, b, X_test)
+
+    d = {
+        "cost": costs,
+        "Y_prediction_test": Y_prediction_test,
+        "Y_prediction_train": Y_prediction_train,
+        "w": w,
+        "b": b,
+        "learning_rate": learning_rate,
+        "num_iterations": num_iterations
+    }
+
+    print("train accuracy: {} %".format(100-np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    print("test accuracy: {} %".format(100-np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+
+    return d
